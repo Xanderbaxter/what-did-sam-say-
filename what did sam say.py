c3f0f9beab03b57037edbf8ccad24ed5250bc60b
Xanderbaxter <xancamo@gmail.com>
2017-08-06 11:45:52 -0400

    added loop so i could continue to talk

--- what did sam say.py
+++ what did sam say.py
@@ -1,10 +1,15 @@
-x=input("what did sam say")
-if "hello" in x:
-    print("hi")
-elif "how are you" in x:
-    print("i'm doing well")
-elif "what is your name" in x:
-    print("my name is hal 9000")
-else:
-    print("Hello")
+print("hello")
+
+while 1:
+    x=input(">")
+    if "hello" in x:
+        print("hi")
+    elif "how are you" in x:
+        print("i'm doing well")
+    elif "what is your name" in x:
+        print("my name is hal 9000")
+    else:
+        print("Hello")
+
+
 
