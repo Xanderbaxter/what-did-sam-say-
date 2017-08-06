7651856182da8fa90d7b3f13b7fbfe322c2836ad
Xanderbaxter <xancamo@gmail.com>
2017-08-06 11:34:05 -0400

    Changed file

---
+++ what did sam say.py
@@ -0,0 +1,10 @@
+x=input("what did sam say")
+if "hello" in x:
+    print("hi")
+elif "how are you" in x:
+    print("i'm doing well")
+elif "what is your name" in x:
+    print("my name is hal 9000")
+else:
+    print("Hello")
+
